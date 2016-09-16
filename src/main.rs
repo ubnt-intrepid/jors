776214e6f2213ed5d2976e7d424f42a05b02728e
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-09-16 19:28:19 +0900

    object case

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 extern crate rustc_serialize;
 
 use std::io::BufRead;
-use rustc_serialize::json::{self,Json};
+use rustc_serialize::json::{self, Json};
 
 fn main() {
-  let stdin = std::io::stdin();
-  let parsed:Vec<_> = stdin.lock().lines().map(|line| Json::from_str(&line.unwrap()).unwrap()).collect();
+  let is_array = {
+    let arg = std::env::args().nth(0).unwrap_or("-a".to_owned());
+    arg == "-a"
+  };
 
-  println!("{}",json::encode(&parsed).unwrap());
+  let stdin = std::io::stdin();
+  if !is_array {
+    for line in stdin.lock().lines() {
+      let parsed: Vec<_> = line.unwrap().split('=').map(|l| l.trim().to_owned()).collect();
+      assert_eq!(parsed.len(), 2);
+
+      let key = parsed[0].clone();
+      let val = Json::from_str(&parsed[1]).unwrap();
+
+      println!("{},{}", key, json::encode(&val).unwrap());
+    }
+  } else {
+    let parsed: Vec<_> = stdin.lock().lines().map(|line| Json::from_str(&line.unwrap()).unwrap()).collect();
+    println!("{}", json::encode(&parsed).unwrap());
+  }
 }
